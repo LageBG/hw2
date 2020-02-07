46c9fba6a051ff3a41016e9e65b12c1b92d753ad
LageBG <60519022+LageBG@users.noreply.github.com>
2020-02-07 15:22:13 -0800

    Create letters.js

---
+++ letters.js
@@ -0,0 +1,244 @@
+function setup() {
+  createCanvas(800, 400);
+}
+
+function printA(x,y) {
+  
+  line(58, 13, 4, 125);
+  line(58, 13, 110, 125);
+  line(4, 125, 110, 125);
+  line(31, 77, 83, 77);
+  
+}
+
+function printB(x,y) {
+  
+  line(128, 13, 128, 124);
+  line(128, 13, 180, 40);
+  line(180, 40, 150, 62);
+  line(150, 62, 207, 90);
+  line(207, 90, 128, 124);
+  
+}
+
+function printC(x,y) {
+  
+  line(277, 13, 223, 66);
+  line(223, 66, 277, 124);
+  
+}
+
+function printD(x,y) {
+  
+  line(318, 13, 318, 124);
+  line(318, 124, 389, 69);
+  line(384, 69, 318, 13);
+  
+}
+
+function printE(x,y) {
+  
+  line(406, 13, 404, 124)
+  line(404, 124, 467, 124)
+  line(405, 69, 443,69)
+  line(406, 13, 454, 13)
+}
+
+function printF(x,y) {
+  
+  line(481, 13, 530, 13)
+  line(481, 13, 480, 125)
+  line(488, 69, 519, 69)
+  
+}
+
+function printG(x,y) {
+  
+  line(639, 31, 594, 18)
+  line(594,18, 558, 50)
+  line(558, 50, 561, 97)
+  line(561, 97, 600, 124)
+  line(600, 124, 644, 93)
+  line(644, 93, 599, 84)
+  line(599, 84, 599, 85)
+  
+}
+
+
+function printH(x,y) {
+  
+  line(678, 15, 678, 124)
+  line(746, 16, 746, 124)
+  line(686, 70, 738, 70)
+  
+}
+    
+function printI(x,y) {
+  
+  line(31, 159, 31, 246)
+  
+}
+
+function printJ(x,y) {
+  
+  line(114, 159, 182, 159)
+  line(146, 159, 146, 246)
+  line(146, 246, 98, 246)
+  line(98, 246, 98, 211)
+  
+}
+
+function printK(x,y) {
+  
+  line(213, 159, 213, 246)
+  line(258, 159, 219,202)
+  line(219, 202, 258, 246)
+  
+}
+
+function printL(x,y) {
+  
+  line(286, 159, 286, 246)
+  line(286, 246, 325, 246)
+  
+}
+
+function printM(x,y) {
+  
+  line(342, 246, 342, 161)
+  line(342, 161, 365, 201)
+  line(365, 201, 388, 161)
+  line(388, 161, 388, 246)
+}
+
+function printN(x,y) {
+  
+  line(415, 246, 415, 161)
+  line(415, 161, 447, 246)
+  line(447, 246, 447, 161)
+}
+
+function printO(x,y) {
+  
+  line(480, 159, 535, 159)
+  line(535, 159, 535, 246)
+  line(535, 246, 480, 246)
+  line(480, 246, 480, 159)
+}
+
+function printO(x,y) {
+  
+  line(480, 159, 535, 159)
+  line(535, 159, 535, 246)
+  line(535, 246, 480, 246)
+  line(480, 246, 480, 159)
+}
+
+function printP(x,y) {
+  
+  line(558, 246, 558, 159)
+  line(558, 159, 606, 159)
+  line(606, 159, 606, 196)
+  line(606, 196, 559, 196)
+}
+
+
+function printQ(x,y) {
+  
+  line(632, 229, 632, 159)
+  line(632, 159, 681, 159)
+  line(681, 159, 681, 229)
+  line(681, 229, 632, 229)
+  line(669, 216, 689, 240)
+}
+
+function printR(x,y) {
+  
+  line(723, 246, 722, 159)
+  line(722, 159, 762, 188)
+  line(762, 188, 735, 210)
+  line(735, 210, 767, 246)
+}
+
+function printS(x,y) {
+  
+  line(73, 263, 24, 300)
+  line(24, 300, 71, 332)
+  line(71, 332, 37, 357)
+}
+
+function printT(x,y) {
+  
+  line(123, 362, 123, 264)
+  line(86, 264, 161, 264)
+  
+}
+
+function printU(x,y) {
+  
+  line(187, 267, 187, 355)
+  line(187, 355, 248, 355)
+  line(248, 355, 248, 267)
+  
+}
+
+
+function printV(x,y) {
+  
+  line(279, 267, 306, 351)
+  line(306, 351, 345, 267)
+  
+}
+
+function printX(x,y) {
+  
+  line(368, 269, 433, 353)
+  line(436, 271, 365, 355)
+  
+}
+
+function printY(x,y) {
+  
+  line(525, 264, 476, 353)
+  line(467, 264, 501, 307)
+  
+}
+
+function printZ(x,y) {
+  
+  line(560, 264, 636, 267)
+  line(636, 267, 555, 351)
+  line(555, 351, 650, 351)
+  
+}
+
+function draw() {
+  background(200);
+
+  printA(10,10);
+  printB(10,10);
+  printC(10,10);
+  printD(10,10);
+  printE(10,10);
+  printF(10,10);
+  printG(10,10);
+  printH(10,10);
+  printI(10,10);
+  printJ(10,10);
+  printK(10,10);
+  printL(10,10);
+  printM(10,10);
+  printN(10,10);
+  printO(10,10);
+  printP(10,10);
+  printQ(10,10);
+  printR(10,10);
+  printS(10,10);
+  printT(10,10);
+  printU(10,10);
+  printV(10,10);
+  printX(10,10);
+  printY(10,10);
+  printZ(10,10);
+  
+}
